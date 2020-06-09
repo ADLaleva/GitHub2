10234084a207dcac8b9995c878cb7474a1e1abca
RoyalFang <GAMihov18@codingburgas.bg>
2020-06-09 19:00:52 +0300

    Make main file and add book structure

---
+++ Library/Library/Library.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+struct Book 
+{
+	int id;
+	string name;
+	string author;
+	string genre;
+	int rating;
+};
+
+int main()
+{
+
+}
