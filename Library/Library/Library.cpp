19ed9e1530acbc2a6123e3acf75507c3a6bc9098
edkehayova18 <edkehayova18@codingburgas.bg>
2020-06-10 22:36:56 +0300

    Add functions "returnBook", "addBook", "ShowBook", "rateBook"

--- Library/Library/Library.cpp
+++ Library/Library/Library.cpp
@@ -3,15 +3,81 @@
 #include <string>
 using namespace std;
 
-struct Book 
+struct BOOK
 {
 	int id;
 	string name;
 	string author;
 	string genre;
 	int rating;
-    //идк
+    
 };
+
+void addBook(BOOK book)
+{
+    cout << "Please " << endl;
+
+    cout << "ID:" << endl;
+    cin >> book.id;
+
+    cout << "Book name:" << endl;
+    cin >> book.name;
+
+    cout << "Author:" << endl;
+    cin >> book.author;
+
+    cout << "Genre:" << endl;
+    cin >> book.genre;
+
+   
+};
+
+void showBook(BOOK book)
+{
+    cout<< book.id << endl;
+    cout << book.name << endl;
+    cout<< book.author << endl;
+    cout<< book.genre << endl;
+    
+    cout<< book.rating << endl;
+};
+
+void rateBook()
+{
+
+
+
+
+};
+
+
+void returnBook(int answer) 
+{
+    cout << "Would you like to rate the book before returning it? (for YES- type 1, for NO- type 0)   " << endl;
+    cin >> answer;
+
+    if (answer != 1 || answer != 0)
+    {
+
+        cout << "Please, input a valid number!" << endl;
+    }
+    else if (answer=1)
+    {
+
+        cout << rateBook;
+    }
+    else
+    {
+        //return the book
+    }
+
+
+
+}
+
+
+
+
 
 int MainMenu() 
 {
