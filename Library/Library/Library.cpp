a4793b622e2c59d392d317fa7a59de04cb21a1f1
ADLaleva18 <ADLaleva18@codingburgas.bg>
2020-06-10 21:31:45 +0300

    Add a small main menu

--- Library/Library/Library.cpp
+++ Library/Library/Library.cpp
@@ -12,6 +12,40 @@ struct Book
 	int rating;
 };
 
+int MainMenu() 
+{
+	int choice;
+
+	cout << "Main Menu\n";
+	cout << "Welcome this is the main menu, please make a choice:\n";
+	cout << "1 - List of books\n";
+	cout << "2 - Add a book\n";
+	cout << "3 - See your ratings\n";
+    cout << "4 - Exit\n";
+	cout << "Selection: ";
+	cin >> choice;
+	selection(choice);
+}
+
+int selection(int selection)
+{
+    switch (selection)
+    {
+    case 1:
+        //List
+        break;
+    case 2:
+        //Add a book
+        break;
+    case 3:
+        //See your ratings
+        break;
+    default:
+        break;
+    }
+
+}
+
 int main()
 {
 
